c8fb8cc323ad60c74338313b2c43a49644c8483e
MerkenY <91915680+MerkenY@users.noreply.github.com>
2022-01-23 18:50:11 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,18 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyAAVUZzMVPp2Q6Pi08J476H6FzGQy9sBHA",
+    authDomain: "class-94-48acd.firebaseapp.com",
+    projectId: "class-94-48acd",
+    storageBucket: "class-94-48acd.appspot.com",
+    messagingSenderId: "380629131323",
+    appId: "1:380629131323:web:b3920a05b6e300db7fbc1e",
+    measurementId: "G-5SLW7PN6BZ"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+function logOut(){
+
+window.location ="index.html";
+
+}
